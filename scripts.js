e45a8dfaa1840bac8f0390545d0d5cc76fb871ab
etitus90 <etitus@protonmail.com>
2022-08-03 12:20:51 -0700

    Create "operate" function that calls others

--- scripts.js
+++ scripts.js
@@ -13,3 +13,15 @@ function multiply(num1, num2) {
 function divide(num1, num2) {
     return (num1 / num2)
 }
+
+function operate(operator, num1, num2) {
+    if (operator === "+") {
+        add(num1, num2)
+    } else if (operator === "-") {
+        subtract(num1, num2)
+    } else if (operator === "*") {
+        multiply(num1, num2)
+    } else if (operator === "/") {
+        divide(num1, num2)
+    }
+}
