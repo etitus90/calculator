92c53cc673c3ef50c234078f88f2d879eb449647
etitus90 <etitus@protonmail.com>
2022-08-08 11:30:46 -0700

    Allow for stringing of operators, prevent / by 0

--- scripts.js
+++ scripts.js
@@ -6,19 +6,32 @@ const equalsBtn = document.getElementById("equals");
 let storedValue = 0;
 let value = 0;
 let operator = "";
+let storedOperator = "";
 
 equalsBtn.addEventListener("click", () => {
      operate(operator, storedValue, value);
+     storedOperator = "";
+     operator = "";
 })
 
 clearBtn.addEventListener("click", () => {
     displayResults.textContent = 0;
+    storedValue = 0;
+    value = 0;
+    operator = "";
+    storedOperator = "";
 });
 
 operatorBtns.forEach(btn => btn.addEventListener("click", () => {
     operator = btn.textContent;
-    storedValue = parseInt(displayResults.textContent);
+    if (storedValue === "0" || storedValue === 0) {
+        storedValue = parseInt(displayResults.textContent);
+    } else {
+        storedValue = parseInt(storedValue);
+        operate(storedOperator, storedValue, value);
+    }
     value = 0;
+    storedOperator = operator;
 }));
 
 numberBtns.forEach(btn => btn.addEventListener("click", () => {
@@ -28,7 +41,6 @@ numberBtns.forEach(btn => btn.addEventListener("click", () => {
         displayResults.textContent += btn.textContent
     }
     value = parseInt(displayResults.textContent)
-    console.log(value)
 }));
 
 
@@ -45,7 +57,11 @@ function multiply(num1, num2) {
 };
 
 function divide(num1, num2) {
-    return (num1 / num2)
+    if (num2 === 0) {
+        alert("Nice try.  No breaking the universe!")
+    } else {
+        return (num1 / num2)
+    }
 };
 
 function operate(operator, num1, num2) {
@@ -53,9 +69,10 @@ function operate(operator, num1, num2) {
         displayResults.textContent = add(num1, num2);
     } else if (operator === "-") {
         displayResults.textContent = subtract(num1, num2);
-    } else if (operator === "*") {
-        displayResults.textContent = multiply(num1, num2)
+    } else if (operator === "x") {
+        displayResults.textContent = multiply(num1, num2);
     } else if (operator === "/") {
-        displayResults.textContent = divide(num1, num2)
+        displayResults.textContent = divide(num1, num2);
     }
+    storedValue = parseInt(displayResults.textContent);
 };
