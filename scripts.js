09440179f39a7c3f26cac31d50c03e1326c2acb4
etitus90 <etitus@protonmail.com>
2022-08-08 11:44:56 -0700

    Finalize code, utilized toFixed(2) in results

--- scripts.js
+++ scripts.js
@@ -45,22 +45,23 @@ numberBtns.forEach(btn => btn.addEventListener("click", () => {
 
 
 function add(num1, num2) {
-    return (num1 + num2)
+    return (num1 + num2).toFixed(2)
 };
 
 function subtract(num1, num2) {
-    return (num1 - num2)
+    return (num1 - num2).toFixed(2)
 };
 
 function multiply(num1, num2) {
-    return (num1 * num2)
+    return (num1 * num2).toFixed(2)
 };
 
 function divide(num1, num2) {
     if (num2 === 0) {
-        alert("Nice try.  No breaking the universe!")
+        alert("Nice try.  No breaking the universe! (Cannot divide by zero!)")
+        return displayResults.textContent = num1;
     } else {
-        return (num1 / num2)
+        return (num1 / num2).toFixed(2)
     }
 };
 
